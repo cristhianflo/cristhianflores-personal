2c977bca88e389e618439ee5b2d97f97301a5528
Cristhian Flores <85910843+cristhianflo@users.noreply.github.com>
2025-12-13 19:52:51 -0400

    feat: populated SkillList component with actual skills and updated CSS

--- src/components/SkillList.tsx
+++ src/components/SkillList.tsx
@@ -1,8 +1,32 @@
 import {
+  siAlpinedotjs,
+  siAstro,
+  siC,
+  siCloudflare,
+  siCss,
+  siDocker,
+  siGin,
+  siGit,
+  siGithubactions,
+  siGo,
+  siHtml5,
+  siJavascript,
+  siJquery,
   siLaravel,
+  siLivewire,
+  siMongodb,
+  siMysql,
+  siNextdotjs,
+  siNodedotjs,
   siPhp,
+  siPocketbase,
+  siPostgresql,
+  siPostman,
   siReact,
+  siTailwindcss,
+  siTypescript,
   siVuedotjs,
+  siWordpress,
   type SimpleIcon as SimpleIconType,
 } from "simple-icons";
 import SimpleIcon from "./SimpleIcon";
@@ -10,13 +34,25 @@ import { useState, type Dispatch, type SetStateAction } from "react";
 
 const SkillCard = ({ icon }: { icon: SimpleIconType }) => {
   return (
-    <div className="border border-amber-400 p-2">
-      <SimpleIcon size={36} icon={icon} />
+    <div
+      title={icon.title}
+      className="group cursor-pointer rounded-md border border-amber-400 p-2 transition-colors hover:bg-amber-400/10"
+    >
+      <SimpleIcon
+        size={48}
+        className="group-hover:text-amber-400"
+        icon={icon}
+      />
     </div>
   );
 };
 
-type ListName = "front-end" | "back-end";
+type ListName =
+  | "languages"
+  | "front-end"
+  | "back-end"
+  | "database"
+  | "dev-tools";
 
 const SkillNavMenu = ({
   onSelect,
@@ -25,33 +61,64 @@ const SkillNavMenu = ({
   onSelect: Dispatch<SetStateAction<ListName>>;
   active: ListName;
 }) => {
+  const listItems: Array<{
+    list: ListName;
+    label: string;
+  }> = [
+    {
+      list: "languages",
+      label: "Lenguajes",
+    },
+    {
+      list: "front-end",
+      label: "Front-end",
+    },
+    {
+      list: "back-end",
+      label: "Back-end",
+    },
+    {
+      list: "database",
+      label: "Bases de datos",
+    },
+    {
+      list: "dev-tools",
+      label: "DevOps",
+    },
+  ];
   return (
-    <div className="h-full w-48 border-r">
-      <ul className="space-y-4 p-4 transition-colors">
+    <ul className="flex w-48 flex-col transition-colors">
+      {listItems.map((item) => (
         <li
-          onClick={() => onSelect("front-end")}
-          className={`cursor-pointer p-2 transition-colors hover:text-amber-400 ${active === "front-end" ? "text-amber-400" : ""}`}
+          onClick={() => onSelect(item.list)}
+          className={`w-full cursor-pointer border-r-2 py-2 pr-8 pl-4 transition-colors hover:text-amber-400 ${active === item.list ? "text-amber-400" : ""}`}
         >
-          Front-end
+          {item.label}
         </li>
-        <li
-          onClick={() => onSelect("back-end")}
-          className={`cursor-pointer p-2 transition-colors hover:text-amber-400 ${active === "back-end" ? "text-amber-400" : ""}`}
-        >
-          Back-end
-        </li>
-      </ul>
-    </div>
+      ))}
+    </ul>
   );
 };
 
 export default function SkillList() {
-  const [activeList, setActiveList] = useState<ListName>("front-end");
+  const [activeList, setActiveList] = useState<ListName>("languages");
   const lists: {
     [key in ListName]: Array<SimpleIconType>;
   } = {
-    "front-end": [siReact, siVuedotjs],
-    "back-end": [siLaravel, siPhp],
+    languages: [siPhp, siGo, siJavascript, siTypescript, siHtml5, siCss, siC],
+    "front-end": [
+      siReact,
+      siVuedotjs,
+      siNextdotjs,
+      siAstro,
+      siLivewire,
+      siAlpinedotjs,
+      siJquery,
+      siTailwindcss,
+    ],
+    "back-end": [siGin, siLaravel, siNodedotjs, siPocketbase, siWordpress],
+    database: [siMysql, siPostgresql, siMongodb],
+    "dev-tools": [siDocker, siGit, siGithubactions, siPostman, siCloudflare],
   };
 
   return (
@@ -60,7 +127,7 @@ export default function SkillList() {
       {Object.entries(lists).map((list) => (
         <div
           key={list[0]}
-          className={`grid-cols-6 gap-4 ${list[0] === activeList ? "grid" : "hidden"}`}
+          className={`w-3/4 flex-wrap content-start items-start gap-4 ${list[0] === activeList ? "flex" : "hidden"}`}
         >
           {list[1].map((icon, index) => (
             <SkillCard key={index} icon={icon} />
